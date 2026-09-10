89f0a380ccc289f658f0241f7037302cead14d9b
Kyle Avrett <kyle@upsurge.consulting>
2026-09-10 14:08:17 -0500

    Setup a uv project

---
+++ README.md
@@ -0,0 +1,3 @@
+# AI Playground
+
+A `uv` project to play around with AI Engineering
---
+++ src/brochure.py
@@ -0,0 +1,8 @@
+from settings import settings
+
+
+def main():
+    print('API_KEY', settings.API_KEY)
+
+if __name__ == '__main__':
+   main()
